f7361361721b402eaf8b6a44457092d3b3d7c14d
thisIsHarshB <harshbalolia2006@gmail.com>
2025-07-26 04:25:47 +0530

    Initial project setup completed.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="font-mulish">
+      <h1 className="text-4xl font-bold text-center mt-10 font-philosopher">
+        Hello Vetra!
+      </h1>
+    </div>
+  )
+}
+
+export default App
